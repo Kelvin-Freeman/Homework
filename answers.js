4794ee1fee1d7047088f3b779ba8042f46273539
Kelvin-Freeman <kelvinlfreeman@live.com>
2022-09-09 23:17:39 -0400

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -37,27 +37,37 @@
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-const sharky = ["Sharky", "shark", 20, "Left Coast"]
-const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
-plantee[2] = 5001
-console.log(plantee);
-wolfy[3] = "Gotham City"
-console.log(wolfy);
-dart[1] = "Hawkins"
-console.log(dart);
-wolfy.shift() 
-console.log(wolfy);
-wolfy.unshift('Gameboy')
-console.log(wolfy);
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+// const sharky = ["Sharky", "shark", 20, "Left Coast"]
+// const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+// const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+// const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+// plantee[2] = 5001
+// console.log(plantee);
+// wolfy[3] = "Gotham City"
+// console.log(wolfy);
+// dart[1] = "Hawkins"
+// console.log(dart);
+// wolfy.shift() 
+// console.log(wolfy);
+// wolfy.unshift('Gameboy')
+// console.log(wolfy);
 
 
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+const turtles = ["Donatello", "Leonardo", "Raphael", "michaelangelo"]
+
+for(let newTurtle of turtles) {
+  let result = newTurtle.toUpperCase()
+console.log(result);
+}
+
+
+
+
 
 
 ////////////////////////////////
